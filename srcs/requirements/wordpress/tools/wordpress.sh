62f9ed5b841eb18bceffe289a97dc47281bca368
Eguelin <eguelin@student.42lyon.fr>
2024-02-28 16:59:39 +0100

    ¯\_(ツ)_/¯

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -3,11 +3,18 @@
 # Install WordPress
 
 # Config Database
-wp core config --path=/var/www/html/wordpress \
-			   --dbname=${DB_NAME} \
-			   --dbuser=${DB_USER} \
-			   --dbpass=${DB_PASSWORD} \
-			   --dbhost=${DB_HOST} --allow-root
+# wp config create --path=/var/www/html/wordpress \
+# 				 --dbname=${DB_NAME} \
+# 				 --dbuser=${DB_USER} \
+# 				 --dbpass=${DB_PASSWORD} \
+# 				 --dbhost=${DB_HOST} --allow-root
+
+cp /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
+sed -i "s/database_name_here/${DB_NAME}/g" /var/www/html/wordpress/wp-config.php
+sed -i "s/username_here/${DB_USER}/g" /var/www/html/wordpress/wp-config.php
+sed -i "s/password_here/${DB_PASSWORD}/g" /var/www/html/wordpress/wp-config.php
+sed -i "s/localhost/${DB_HOST}/g" /var/www/html/wordpress/wp-config.php
+
 
 # Install WordPress
 wp core install --path=/var/www/html/wordpress \
@@ -20,9 +27,10 @@ wp core install --path=/var/www/html/wordpress \
 # Create User
 wp user create ${WP_USER} \
 			   ${WP_USER_EMAIL} \
-			   --role=author \
+			   --role=editor \
 			   --user_pass=${WP_USER_PASSWORD} \
-			   --path=/var/www/html/wordpress --allow-root
+			   --path=/var/www/html/wordpress
 
 # Start PHP
+mkdir -p /run/php
 php-fpm7.4 -g "daemon off;"
