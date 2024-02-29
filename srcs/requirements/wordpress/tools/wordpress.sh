12b3bab9867a285343412f34e2f4765f908ee3fa
Eguelin <eguelin@student.42lyon.fr>
2024-02-29 14:12:57 +0100

    no error but it doesn't work

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-# Install WordPress
-
 if [ ! -d /var/www/html/wordpress ]; then
 	# Install WordPress
 	wp core download --path=/var/www/html/wordpress --allow-root
 
 	# Config Database
-	# wp config create --path=/var/www/html/wordpress \
-	# 				 --dbname=${DB_NAME} \
-	# 				 --dbuser=${DB_USER} \
-	# 				 --dbpass=${DB_PASSWORD} \
-	# 				 --dbhost=${DB_HOST} --allow-root
-
 	cp /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
 	sed -i "s/database_name_here/${DB_NAME}/g" /var/www/html/wordpress/wp-config.php
 	sed -i "s/username_here/${DB_USER}/g" /var/www/html/wordpress/wp-config.php
@@ -26,14 +18,15 @@ if [ ! -d /var/www/html/wordpress ]; then
 					--title=${WP_TITLE} \
 					--admin_user=${WP_ADMIN} \
 					--admin_password=${WP_ADMIN_PASSWORD} \
-					--admin_email=${WP_ADMIN_EMAIL} --allow-root
-
+					--admin_email=${WP_ADMIN_EMAIL} \
+					--allow-root
 	# Create User
-	wp user create ${WP_USER} \
-				${WP_USER_EMAIL} \
-				--role=editor \
-				--user_pass=${WP_USER_PASSWORD} \
-				--path=/var/www/html/wordpress
+	wp user create ${WP_USER}\
+				   ${WP_USER_EMAIL} \
+				   --role=editor \
+				   --user_pass=${WP_USER_PASSWORD} \
+				   --path=/var/www/html/wordpress \
+				   --allow-root
 fi
 
 # Start PHP
