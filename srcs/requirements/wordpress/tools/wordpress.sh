ad4738d734fb0ce6ecc0307dfb39a3029c28fd4d
Egurlin <eguelin@student.42lyon.fr>
2024-09-06 16:07:25 +0200

    refactor: remove sendmail

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -24,7 +24,9 @@ if [ ! -d /var/www/html/wordpress ]; then
 							 --admin_user=${WP_ADMIN} \
 							 --admin_password=${WP_ADMIN_PASSWORD} \
 							 --admin_email=${WP_ADMIN_EMAIL} \
+							 --skip-email \
 							 --allow-root
+
 	# Create User
 	wp-cli.phar user create ${WP_USER}\
 							${WP_USER_EMAIL} \
